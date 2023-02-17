4c680a2119f424f792460fc7ff0033e50f370551
Mohammed Essam <memaq1998.10.7@gmail.com>
2023-02-18 01:03:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+let names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, event) {
+    let printCard = [];
+
+    for (let i = 0; i < names.length; i++) {
+        printCard.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return printCard;
+}
+function countDown(number) {
+    while (number >= 0)
+        console.log(number--);
+}
+
+console.log(writeCards(names, "surprise"));
+countDown(10);
+countDown(4);
